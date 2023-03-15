b6852072e11436c0bc80eff55324262708e5b7cc
dinacazun <87436886+dinacazun@users.noreply.github.com>
2023-03-15 01:31:23 -0400

    Update ReverseVowels.py

--- Assignment-1/ReverseVowels.py
+++ Assignment-1/ReverseVowels.py
@@ -1,1 +1,21 @@
+def reverseVowels(s):
+    
+    vowels = set(list("aeiouAEIOU"))
+    
+    s = list(s)
+    
+    i = 0
+    j = len(s) - 1
+    
+    while i < j:
+        if(s[i] in vowels and s[j] in vowels):
+            s[i] = s[j]
+            s[j] = s[i]
+          
+        i += 1  
+        j -= 1
+            
+    return "".join(s)
 
+
+print(reverseVowels("flamingo"))
