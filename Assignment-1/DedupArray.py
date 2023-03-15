ebc2706a4ff72b8aeb2701ff2c9c22c90a53d127
dinacazun <87436886+dinacazun@users.noreply.github.com>
2023-03-15 01:37:21 -0400

    Update DedupArray.py

--- Assignment-1/DedupArray.py
+++ Assignment-1/DedupArray.py
@@ -1,1 +1,18 @@
-
+#Hash the Elements?
+def dedupArray(arr):
+    
+    #returning a new list, so create new list as empty
+    #iterate through given arr and if element is not in the new list, add it to new list
+    #if we have seen it (meaning duplicates), then we don't add it
+    #return new list
+    #iterate through array and put each number into hashmap
+    
+    newArray = []
+    
+    for element in arr:
+        if element not in newArray:
+            newArray.append(element)
+    
+    return newArray
+    
+print(dedupArray([1, 3, 4, 8, 10, 12]))
