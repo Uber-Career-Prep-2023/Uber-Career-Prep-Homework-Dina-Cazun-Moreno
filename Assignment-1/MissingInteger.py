3dd5748a5d3c2a864d15bfc487a894c44c8a1394
dinacazun <87436886+dinacazun@users.noreply.github.com>
2023-03-15 01:35:36 -0400

    Update MissingInteger.py

--- Assignment-1/MissingInteger.py
+++ Assignment-1/MissingInteger.py
@@ -1,1 +1,22 @@
+#Hash the elements
 
+def missingInteger(arr, n):
+    
+    #create hashmap of all nums 1 - n. set frequency or count of each element to 0.
+    #key will num. value will frequency
+    #go through array and have counter or frequency increment if num in array is in hashmap
+    #if any key's value in hashmap is 0 then return that key
+    hashmap = {}
+    
+    for i in range(1, n+1):
+        hashmap[i] = 0
+        
+    for i in range(len(arr)):
+        if arr[i] in hashmap:
+            hashmap[arr[i]] += 1
+    
+    for key in hashmap:
+        if hashmap[key] == 0:
+            return key
+        
+print(missingInteger([1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12], 12))
