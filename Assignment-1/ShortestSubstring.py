bb8218c6b6cf3d510f1b8a35cb70c2eb1cff16a9
dinacazun <87436886+dinacazun@users.noreply.github.com>
2023-03-15 01:34:57 -0400

    Update ShortestSubstring.py

--- Assignment-1/ShortestSubstring.py
+++ Assignment-1/ShortestSubstring.py
@@ -1,1 +1,16 @@
+#Growing/Shrinking Sliding Window
 
+def shortestSubstring(s1, s2):
+    #need to keep track of running substrings to then find min of them
+    
+    #hash the elements to have a counter of them
+    #have two pointers i and j
+    
+    a, b = len(s), len(t)
+    if a < b: 
+        return " "
+        
+    hmap = collection.Counter(s2)
+    
+    for i in range(len(s1)):
+        #if we have not seen character in substring then keep going
