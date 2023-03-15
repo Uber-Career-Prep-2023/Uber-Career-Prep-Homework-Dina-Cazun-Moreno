ffc4617a072556655a5cb6018dd85cf807264df8
dinacazun <87436886+dinacazun@users.noreply.github.com>
2023-03-15 01:37:54 -0400

    Update TwoSum.py

--- Assignment-1/TwoSum.py
+++ Assignment-1/TwoSum.py
@@ -1,1 +1,15 @@
-
+#Sliding Window and Hashing Elements
+def twoSum(arr, k):
+    
+    #sliding window because the window will be 2 since we are looking for pairs
+    #find the matching element to the sum with reference to the current num at index
+    #if it is in the hashmap, then we output that pair: the num at the index and the matching element
+    
+    hashmap = {}
+    
+    for i in range(len(arr)):
+        matchingElement = k - arr[i]
+        if difference in hashmap:
+            print("Pairs: " "(", arr[i], ",", difference,")")
+    
+print(twoSum([1, 10, 8, 3, 2, 5, 7, 2, -2, -1], 10))
