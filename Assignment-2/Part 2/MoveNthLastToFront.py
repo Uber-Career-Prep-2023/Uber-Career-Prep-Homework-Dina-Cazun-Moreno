4cad40e2d4dbe43729e8f09ede08b2197d99b45d
dinacazun <87436886+dinacazun@users.noreply.github.com>
2023-05-03 00:43:05 -0400

    Create MoveNthLastToFront.py

---
+++ Assignment-2/Part 2/MoveNthLastToFront.py
@@ -0,0 +1,11 @@
+#since we can only move one way in a linked list, both pointers will be
+#at a fixed distance once they start moving
+#Linked List Fixed Distance Two-Pointer
+
+class Node:
+    def _init_(self, data):
+        self.data = data
+        self.next = None
+
+    def moveNthLastToFront(head, k):
+        return 
