883b14eea18e97b940f3952a9db3b4b2a3498666
dinacazun <87436886+dinacazun@users.noreply.github.com>
2023-06-21 01:55:41 -0400

    Create Question 11.py

---
+++ Assignment-3/Part 2/Question 11.py
@@ -0,0 +1,29 @@
+import queue
+
+def vacationDestinations(destinations, origin, k):
+    graph = {}
+    for(u, v, w) in destinations:
+        if u not in graph:
+            graph[u] = {}
+        if v not in graph:
+            graph[v] = {}
+        graph[u][v] = w
+        graph[v][u] = w
+    q = queue.Queue()
+    q.put((origin, 0))
+    visited = set()
+    while not q.empty():
+        (u, t) = q.get()
+        if t > k:
+            break
+        visited.add(u)
+        for v in graph[u]:
+            if v not in visited:
+                q.put((v, t + graph[u][v] + 1))
+    return sorted(list(visited - set([origin])))
+
+destinations = [("Boston", "New York", 4), ("New York", "Philadelphia.", 2), ("Boston", "Newport", 1.5), ("Washington, D.C.", "Harper's Ferry", 1), ("Boston", "Portland", 2.5), ("Philadelphia", "Washington, D.C.", 2.5)]
+origin = "New York"
+k = 5
+
+print(vacationDestinations(destinations, origin, k))
