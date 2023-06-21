9e3d1e9bb336cf3dfd331b0166d752406d54dd75
dinacazun <87436886+dinacazun@users.noreply.github.com>
2023-06-21 01:47:19 -0400

    Create Question 3.py

---
+++ Assignment-3/Part 1/Question 3.py
@@ -0,0 +1,26 @@
+import heapq
+
+class PriorityQueue:
+    def __init__(self):
+        self.arr = []
+    
+    def top(self):
+        return self.arr[0][1]
+    
+    def insert(self, x, weight):
+        heapq.heappush(self.arr, (weight, x))
+
+    def remove(self):
+        heapq.heappop(self.arr)
+
+if __name__ == '__main__':
+    pq = PriorityQueue()
+    pq.insert(5, 1)
+    pq.insert(6, 2)
+    
+    print("this is my priority queue: ")
+    print(pq) #{<__main__.PriorityQueue object at 0x7f4b4f1a2590>}
+    print("----------")
+    print(pq.top()) #{5}
+    pq.remove()
+    print(pq.top()) #{6}
