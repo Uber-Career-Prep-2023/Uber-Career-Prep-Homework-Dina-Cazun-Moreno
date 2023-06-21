c0487a458223079efc5a7288f67af4299d8b1bb2
dinacazun <87436886+dinacazun@users.noreply.github.com>
2023-06-21 01:54:26 -0400

    Create Question 8.py

---
+++ Assignment-3/Part 2/Question 8.py
@@ -0,0 +1,20 @@
+def alternatingPath(graph, origin, destination):
+    queue = deque([origin, 0, None])
+    visited = set()
+    while queue:
+        node, length, color = queue.popleft()
+        if node == destination:
+            return length
+        visited.add((node, color))
+        for neighbor, edge_color in graph[node]:
+            if (neighbor, edge_color) not in visited and edge_color != color:
+                queue.append((neighbor, length+1, edge_color))
+    return -1
+
+graph = {
+    'A': [('B', 'blue'), ('C', 'red'), ('D', 'red')],
+    'B': [('D', 'blue'), ('E', 'blue')],
+    'C': [('B', 'red')],
+    'D': [('C', 'blue'), ('E', 'red')],
+    'E': [('C', 'red')]
+}
