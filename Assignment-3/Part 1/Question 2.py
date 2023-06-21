38022b78fe8bc745c3235a8c97cab8fb0395d69a
dinacazun <87436886+dinacazun@users.noreply.github.com>
2023-06-21 01:44:33 -0400

    Create Question 2.py

---
+++ Assignment-3/Part 1/Question 2.py
@@ -0,0 +1,31 @@
+import heapq
+
+class Heap:
+    def __init__(self):
+        self.arr = []
+    
+    def top(self):
+        return self.arr[0]
+    
+    def insert(self, x):
+        heapq.heappush(self.arr, x)
+
+    def remove(self):
+        heapq.heappop(self.arr)
+
+if __name__ == '__main__':
+    myHeap = Heap()
+    myHeap.insert(5)
+    myHeap.insert(6)
+    # myHeap.insert(7)
+    # myHeap.insert(9)
+    # myHeap.insert(13)
+    # myHeap.insert(11)
+    # myHeap.insert(10)
+    
+    print("this is my heap: ")
+    print(myHeap) #{<__main__.Heap object at 0x7f25dd2a24d0>}
+    print("----------")
+    print(myHeap.top()) #{5}
+    myHeap.remove()
+    print(myHeap.top()) #{6}
