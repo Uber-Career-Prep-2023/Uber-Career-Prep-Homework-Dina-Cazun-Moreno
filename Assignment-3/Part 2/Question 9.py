14cabd740281977b6be811dd16d3589c8aa05f68
dinacazun <87436886+dinacazun@users.noreply.github.com>
2023-06-21 22:13:16 -0400

    Update Question 9.py

--- Assignment-3/Part 2/Question 9.py
+++ Assignment-3/Part 2/Question 9.py
@@ -15,3 +15,6 @@ def mergeKSortedArrays(k, arr):
     return res
 
 print(mergeKSortedArrays(2, [[1, 2, 3, 4, 5], [1, 3, 5, 7, 9]]))
+#output: [1, 1, 2, 3, 3, 4, 5, 5, 7, 9]
+print(mergeKSortedArrays(3, [[1, 4, 7, 9], [2, 6, 7, 10, 11, 13, 15], [3, 8, 12, 13, 16]]))
+#output: [1, 2, 3, 4, 6, 7, 7, 8, 9, 10, 11, 12, 13, 13, 15, 16]
