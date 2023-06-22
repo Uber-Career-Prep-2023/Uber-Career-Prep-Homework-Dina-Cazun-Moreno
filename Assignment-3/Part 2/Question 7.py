3a5ed5842f46c2214dab380f4bb065c4f0c288cf
dinacazun <87436886+dinacazun@users.noreply.github.com>
2023-06-21 22:08:52 -0400

    Update Question 7.py

--- Assignment-3/Part 2/Question 7.py
+++ Assignment-3/Part 2/Question 7.py
@@ -1,3 +1,8 @@
 def reverseWords(s):
     words = s.split()
     return ' '.join(words[::-1])
+
+print(reverseWords("Uber Career Prep"))
+#output: Prep Career Uber
+print(reverseWords("Emma lives in Brooklyn, New York."))
+#output: York. New Brooklyn, in lives Emma
