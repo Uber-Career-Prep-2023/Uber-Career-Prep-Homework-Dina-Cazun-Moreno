e52f077b94da6c34863acae1bfdf77c388356494
dinacazun <87436886+dinacazun@users.noreply.github.com>
2023-06-22 22:28:57 -0400

    Update CopyTree.py

--- Assignment-2/Part 2/CopyTree.py
+++ Assignment-2/Part 2/CopyTree.py
@@ -5,17 +5,34 @@
 #returns root of new tree
 
 class BinarySearchTree:
-    def _init_(self, data):
+    def _init_(self, left, right, data):
         self.data = data
         self.left = None
         self.right = None
 
-    def copyTree(root):
-        if root is None:
-            return None
+def copyTree(root):
+    if not root:
+        return None
         
-        copy = BinarySearchTree(root.data)
-        copy.left = copyTree(root.left)
-        copy.right = copyTree(root.right)
+    copy = BinarySearchTree(root.data)
+    copy.left = copyTree(root.left)
+    copy.right = copyTree(root.right)
 
-        return copy
+    return copy
+
+if __name__ == '__main__':
+ 
+    root = BinarySearchTree(1)
+    root.left = BinarySearchTree(2)
+    root.right = BinarySearchTree(3)
+    root.left.left = BinarySearchTree(4)
+    root.left.right = BinarySearchTree(5)
+    root.right.left = BinarySearchTree(6)
+    root.right.right = BinarySearchTree(7)
+ 
+    clone = copyTree(root)
+ 
+    print('This is new tree: ')
+    print(clone.val)
+    
+#output: prints 1, which is root of new copy of tree
