78ad8bd4f62959953ff42e038d847ef20462dda1
dinacazun <87436886+dinacazun@users.noreply.github.com>
2023-06-22 22:38:25 -0400

    Update IsBST.py

--- Assignment-2/Part 2/IsBST.py
+++ Assignment-2/Part 2/IsBST.py
@@ -4,18 +4,31 @@
 #inorder: left, root, right
 #return boolean
 
-def isBST(root) -> bool:
-    nodes = []
-    
-    def inorderTraversal(root):
-        if root is None:
-            return None
-        inorderTraversal(root.left)
-        nodes.append(root)
-        inorderTraversal(root.right)
-    
-    inorderTraversal(root)
-    if nodes == list(sorted(set(nodes))):
+class Node:
+    def __init__(self, data):
+        self.left = None
+        self.right = None
+        self.data = data
+
+def isBST(node, minVal = float('-inf'), maxVal = float('inf')):
+    if node is None:
         return True
-    else:
+    if node.data < minVal or node.data > maxVal:
         return False
+    return isBST(node.left, minVal, node.data - 1) and isBST(node.right, node.data + 1, maxVal)
+
+if __name__ == "__main__":
+  root = Node(4)
+  root.left = Node(2)
+  root.right = Node(5)
+  # root.right.left = Node(7)
+  root.left.left = Node(1)
+  root.left.right = Node(3)
+ 
+  # Function call
+  if is_bst(root) is True:
+      print("Is BST")
+  else:
+      print("Not a BST")
+
+#output: Is BST
