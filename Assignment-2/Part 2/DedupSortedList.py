69339360b666aa88ffbe448590ad02e78915f15a
dinacazun <87436886+dinacazun@users.noreply.github.com>
2023-06-22 22:48:35 -0400

    Update DedupSortedList.py

--- Assignment-2/Part 2/DedupSortedList.py
+++ Assignment-2/Part 2/DedupSortedList.py
@@ -2,19 +2,23 @@
 #Hash Linked List Nodes technique
 
 class Node:
-    def _init_(self, data):
+    def _init_(self, data, next):
         self.data = data
         self.next = None
 
-    def dedupSortedList(head):
-        curr = head
-        prev = None
-        duplicateCounter = dict()
-        while curr:
-            if curr.data not in duplicateCounter:
-                duplicateCounter[curr.data] = None
-                prev = curr
-            else:
-                prev.next = curr.next
-
+def dedupSortedList(head):
+    curr = head
+    while curr and curr.next:
+        if curr.data == curr.next.data:
+            curr.next = curr.next.next
+        else:
             curr = curr.next
+    return head
+
+if __name__ == "__main__":
+  root = Node(1, root.next)
+  root.next = Node(2, root.next.next)
+  root.next.next = Node(2)
+  print(root)
+
+#Trying to test but output says root is not defined?
