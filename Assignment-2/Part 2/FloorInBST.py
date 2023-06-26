725fc7707673fab56634f4beb09d76d52dcad8c3
dinacazun <87436886+dinacazun@users.noreply.github.com>
2023-06-26 00:19:28 -0400

    Update FloorInBST.py

--- Assignment-2/Part 2/FloorInBST.py
+++ Assignment-2/Part 2/FloorInBST.py
@@ -1,22 +1,50 @@
-class BinarySearchTree:
-    def _init_(self, data):
-        self.data = data
+class newNode:
+    def __init__(self, data):
+        self.key = data
         self.left = None
         self.right = None
 
-    global floor
+def insert(node, key):
+     
+    if node == None:
+        return newNode(key)
+ 
+    if key < node.key:
+        node.left = insert(node.left, key)
+    elif key > node.key:
+        node.right = insert(node.right, key)
+         
+    return node
+ 
+def findMaxforN(root, N):
+    if root == None:
+        return -1
+    if root.key == N:
+        return N
 
-    def floorInBSTHelper(root, target):
-        while root:
-            if root.data == target:
-                floor = root.data
-            if target > root.data:
-                floor = root.data
-                root = root.right
-            else:
-                root = root.left
+    elif root.key < N:
+        k = findMaxforN(root.right, N)
+        if k == -1:
+            return root.key
+        else:
+            return k
+ 
+    elif root.key > N:
+        return findMaxforN(root.left, N)
+ 
+if __name__ == '__main__':
+    N = 4
+ 
+    root = None
+    root = insert(root, 25)
+    insert(root, 2)
+    insert(root, 1)
+    insert(root, 3)
+    insert(root, 12)
+    insert(root, 9)
+    insert(root, 21)
+    insert(root, 19)
+    insert(root, 25)
+    print(findMaxforN(root, N))
 
-    def floorInBST(root, target):
-        floor -= 1
-        floorInBSTHelper(root, target)
-        print(target, floor)
+#Output: 3
