9b10ed5d156ac66ff24cdbc48fcea0278d9c2aff
dinacazun <87436886+dinacazun@users.noreply.github.com>
2023-06-25 23:45:29 -0400

    Update DisconnectCycle.py

--- Assignment-2/Part 2/DisconnectCycle.py
+++ Assignment-2/Part 2/DisconnectCycle.py
@@ -2,20 +2,75 @@
 #Hash Linked List Nodes
 
 class Node:
-    def _init_(self, data):
+    def __init__(self, data):
         self.data = data
         self.next = None
+ 
+class LinkedList:
+    def __init__(self):
+        self.head = None
+ 
+    def detectAndRemoveLoop(self):
+        slow_p = fast_p = self.head
+         
+        while(slow_p and fast_p and fast_p.next):
+            slow_p = slow_p.next
+            fast_p = fast_p.next.next
 
-    def disconnectCycle(head):
-        curr = head
-        prev = None
+            if slow_p == fast_p:
+                self.removeLoop(slow_p)
+         
+                return 1
+         
+        return 0
+ 
+    def removeLoop(self, loop_node):
+        ptr1 = loop_node
+        ptr2 = loop_node
+         
+        k = 1
+        while(ptr1.next != ptr2):
+            ptr1 = ptr1.next
+            k += 1
+ 
+        ptr1 = self.head
+         
+        ptr2 = self.head
+        for i in range(k):
+            ptr2 = ptr2.next
+ 
+        while(ptr2 != ptr1):
+            ptr1 = ptr1.next
+            ptr2 = ptr2.next
+ 
+        while(ptr2.next != ptr1):
+            ptr2 = ptr2.next
+ 
+        ptr2.next = None
+ 
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+ 
+    def printList(self):
+        temp = self.head
+        while(temp):
+            print(temp.data, end = ' ')
+            temp = temp.next
 
-        nodes = set()
 
-        while curr:
-            if curr in nodes:
-                prev.next = None
-                return
-            nodes.add(curr)
-            prev = curr
-            curr = curr.next
+llist = LinkedList()
+llist.push(10)
+llist.push(4)
+llist.push(15)
+llist.push(20)
+llist.push(50)
+ 
+llist.head.next.next.next.next.next = llist.head.next.next
+ 
+llist.detectAndRemoveLoop()
+ 
+print("Linked List after removing loop")
+print(mylist.printList())
+#Output: 50 20 15 4 10
