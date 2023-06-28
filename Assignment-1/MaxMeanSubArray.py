db8aa7e73a0abaef01473090b515b320fdc0c797
dinacazun <87436886+dinacazun@users.noreply.github.com>
2023-06-27 21:19:46 -0400

    Update MaxMeanSubArray.py

--- Assignment-1/MaxMeanSubArray.py
+++ Assignment-1/MaxMeanSubArray.py
@@ -3,20 +3,23 @@
 #mean = average of numbers. since k is fixes, max sum gives us the highest mean sum
 
 def maxMeanSubArray(arr, k):
+    n = len(arr)
+    if k > n:
+        return None
     
-    if k > len(arr):
-        return -1
+    max_mean = 0
+    curr_sum = sum(arr[:k])
     
-    maxSum = 0
+    for i in range(n - k + 1):
+        curr_mean = curr_sum / k
+        max_mean = max(max_mean, curr_mean)
+        
+        if i + k < n:
+            curr_sum = curr_sum - arr[i] + arr[i + k]
     
-    for i in range(len(arr)-k+1):
-        currentSum = arr[i] + arr[i+k-1]
-        if currentSum > maxSum:
-            maxSum = currentSum
-            
-    return maxSum/k
+    return max_mean
     
-print(maxMeanSubArray([4, 5, -3, 2, 6, 1], 2))
-
-#Output
-#4.5
+print(maxMeanSubArray([4, 5, -3, 2, 6, 1], 2)) #Output: 4.5
+print(maxMeanSubArray([4, 5, -3, 2, 6, 1], 3)) #Output: 3.0
+print(maxMeanSubArray([1, 1, 1, 1, -1, -1, 2, -1, -1], 3)) #Output: 1.0
+print(maxMeanSubArray([1, 1, 1, 1, -1, -1, 2, -1, -1, 6], 5)) #Output: 1.0
