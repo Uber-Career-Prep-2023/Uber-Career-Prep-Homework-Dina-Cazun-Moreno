bb067f655c3b59c0f8e1d6892b3ab5f229b19e5e
dinacazun <87436886+dinacazun@users.noreply.github.com>
2023-06-27 21:22:56 -0400

    Update ReverseVowels.py

--- Assignment-1/ReverseVowels.py
+++ Assignment-1/ReverseVowels.py
@@ -1,25 +1,22 @@
 #Two Pointer
 def reverseVowels(s):
-    
-    vowels = set(list("aeiouAEIOU"))
-    
+    vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
     s = list(s)
-    
-    i = 0
-    j = len(s) - 1
-    
+    i, j = 0, len(s) - 1
     while i < j:
-        if(s[i] in vowels and s[j] in vowels):
-            s[i] = s[j]
-            s[j] = s[i]
-          
-        i += 1  
-        j -= 1
-            
-    return "".join(s)
-
-
-print(reverseVowels("flamingo"))
-
-#Output
-#flamingo
+        if s[i] in vowels and s[j] in vowels:
+            s[i], s[j] = s[j], s[i]
+            i += 1
+            j -= 1
+        elif s[i] in vowels:
+            j -= 1
+        elif s[j] in vowels:
+            i += 1
+        else:
+            i += 1
+            j -= 1
+    return ''.join(s)
+    
+print(reverseVowels("Uber Career Prep")) #Output: eber Ceraer prUp
+print(reverseVowels("xyz")) #Output: xyz
+print(reverseVowels("flamingo")) #Output: flominga
