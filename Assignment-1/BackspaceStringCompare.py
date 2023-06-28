1eac4d34d640519c0458986d5f5d9f93bb438aa1
dinacazun <87436886+dinacazun@users.noreply.github.com>
2023-06-27 21:27:59 -0400

    Update BackspaceStringCompare.py

--- Assignment-1/BackspaceStringCompare.py
+++ Assignment-1/BackspaceStringCompare.py
@@ -1,27 +1,19 @@
 #Simultaneous Iteration - Two Pointer
 
-def backspaceStringCompare(s1, s2):
+def backspaceStringCompare(s: str, t: str) -> bool:
+    def build_string(string: str) -> str:
+        stack = []
+        for char in string:
+            if char != '#':
+                stack.append(char)
+            elif stack:
+                stack.pop()
+        return ''.join(stack)
     
-    #save s1 and s2 into list array bc strings are immutable
+    return build_string(s) == build_string(t)
     
-    stringOne = list(s1).reverse()
-    stringTwo = list(s2).reverse()
-    
-    if stringOne == stringTwo:
-        return True
-    else:
-        for i in range(len(stringOne)):
-            for j in range(len(stringTwo)):
-                if stringTwo[j] == "#":
-                    del stringTwo[j]
-                    del stringTwo[j+1]
-                if stringOne == stringTwo:
-                    return True
-                else:
-                    return False
-    
+print(backspaceStringCompare("abcde", "abcde"))
 print(backspaceStringCompare("Uber Career Prep", "u#Uber Careee#r Prep"))
-print(backspaceStringCompare("poop", "poop"))
 print(backspaceStringCompare("abcdef###xyz", "abcw#xyz"))
 print(backspaceStringCompare("abcdef###xyz", "abcdefxyz###"))
 
@@ -29,4 +21,4 @@ print(backspaceStringCompare("abcdef###xyz", "abcdefxyz###"))
 #True
 #True
 #True
-#True
+#False
