375d664a24603cb4776aa0451fb9c425025ee215
dinacazun <87436886+dinacazun@users.noreply.github.com>
2023-06-30 19:11:49 -0400

    Update MergeIntervals.py

--- Assignment-1/MergeIntervals.py
+++ Assignment-1/MergeIntervals.py
@@ -1,30 +1,19 @@
 #Sort, then Solve
-def mergeIntervals(arr):
+def mergeIntervals(intervals):
+    if not intervals:
+        return []
     
-    #first sort the intervals so that it's easier to look at and iterate over
-    #make new intervals for merged intervals
-    #start and end variables are at the first item in intervals
-    #create different start and end variables to traverse through each item 
+    intervals.sort(key=lambda x: x[0])
+    merged = [intervals[0]]
     
-    intervals = sorted(arr)
-    newIntervals = []
-    
-    start = intervals[0]
-    end = intervals [0]
-    
-    for s, e in intervals[1:]:
-        if s > end:
-            newIntervals.append((start, end))
-            start = s
-            end = e
+    for interval in intervals[1:]:
+        if interval[0] <= merged[-1][1]:
+            merged[-1][1] = max(merged[-1][1], interval[1])
         else:
-            
+            merged.append(interval)
     
-    return intervals
-        
-print(mergeIntervals([(2, 3), (4, 8), (1, 2), (5, 7), (9, 12)]))
-
-#Output
-#File "/Users/dinacazun/UCP_HW1.py", line 22
-    #return intervals
-#IndentationError: expected an indented block
+    return merged
+    
+print(mergeIntervals([(2, 3), (4, 8), (1, 2), (5, 7), (9, 12)])) #Output: [(4, 8), (1, 3), (9, 12)]
+print(mergeIntervals([(5, 8), (6, 10), (2, 4), (3, 6)]) #Output: [(2, 10)]
+print(mergeIntervals([(10, 12), (5, 6), (7, 9), (1, 3)]) #Output: [(10, 12), (5, 6), (7, 9), (1, 3)]
