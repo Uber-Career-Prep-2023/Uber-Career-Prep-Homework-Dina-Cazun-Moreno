64d27a72839b1b692c968461e683dd7a7bc782b2
dinacazun <87436886+dinacazun@users.noreply.github.com>
2023-06-30 19:18:23 -0400

    Update TwoSum.py

--- Assignment-1/TwoSum.py
+++ Assignment-1/TwoSum.py
@@ -1,23 +1,17 @@
 #Sliding Window and Hashing Elements
-def twoSum(arr, k):
+def twoSum(nums, k):
+    count = 0
+    num_dict = {}
     
-    #sliding window because the window will be 2 since we are looking for pairs
-    #find the matching element to the sum with reference to the current num at index
-    #if it is in the hashmap, then we output that pair: the num at the index and the matching element
+    for num in nums:
+        complement = k - num
+        if complement in num_dict:
+            count += num_dict[complement]
+        num_dict[num] = num_dict.get(num, 0) + 1
     
-    hashmap = {}
+    return count
     
-    for i in range(len(arr)):
-        matchingElement = k - arr[i]
-        if difference in hashmap:
-            print("Pairs: " "(", arr[i], ",", difference,")")
-    
-print(twoSum([1, 10, 8, 3, 2, 5, 7, 2, -2, -1], 10))
-
-#Output
-#Traceback (most recent call last):
-  #File "/Users/dinacazun/UCP_HW1.py", line 15, in <module>
-    #print(twoSum([1, 10, 8, 3, 2, 5, 7, 2, -2, -1], 10))
-  #File "/Users/dinacazun/UCP_HW1.py", line 12, in twoSum
-    #if difference in hashmap:
-#NameError: name 'difference' is not defined
+print(twoSum([1, 10, 8, 3, 2, 5, 7, 2, -2, -1], 10)) #Output: 3
+print(twoSum([1, 10, 8, 3, 2, 5, 7, 2, -2, -1], 9)) #Output: 4
+print(twoSum([4, 3, 3, 5, 7, 0, 2, 3, 8, 6], 6)) #Output: 5
+print(twoSum([4, 3, 3, 5, 7, 0, 2, 3, 8, 6], 1)) #Output: 0
