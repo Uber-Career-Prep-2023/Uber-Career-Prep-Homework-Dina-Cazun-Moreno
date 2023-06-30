e2f9cf296addd172856dad0f3a5ef028e37b248c
dinacazun <87436886+dinacazun@users.noreply.github.com>
2023-06-30 19:07:41 -0400

    Update KAnagrams.py

--- Assignment-1/KAnagrams.py
+++ Assignment-1/KAnagrams.py
@@ -1,39 +1,22 @@
-def kAnagrams(s1, s2, k):
-    
-    #create hashmap of the characters of first string. set frequency or count of each char to 1.
-    #key will char. value will be frequency.
-    #go through second string and have counter or frequency decrement if char in second string is in hashmap
-    #if hashmap's values added together are greater than k, then not anagrams.
-    
-    hashmap = {}
-    counterValue = 0
-    frequency = 0
-    
-    for i in range(len(s1)):
-        hashmap[s1[i]] = 1 #key and value assignment
-        if hashmap[s1[i]] in hashmap:
-            hashmap[s1[i]] += 1
-    
-    print(hashmap)
-        
-    for i in range(len(s2)):
-        if s2[i] in hashmap:
-            hashmap[s2[i]] -= 1
-            
-    print(hashmap)
-            
-    for value in hashmap.values():
-        if value == 1:
-            counterValue += 1
-            
-    if counterValue > k:
+def kAnagrams(str1, str2, k):
+    if len(str1) != len(str2):
         return False
-    else:
-        return True
-        
-print(kAnagrams("apple", "peach", 1))
-
-#Output
-#{'a': 1, 'p': 1, 'l': 1, 'e': 1}
-#{'a': 0, 'p': 0, 'l': 1, 'e': 0}
-#True
+    
+    count = 0
+    freq = [0] * 26
+    
+    for i in range(len(str1)):
+        freq[ord(str1[i]) - ord('a')] += 1
+        freq[ord(str2[i]) - ord('a')] -= 1
+    
+    for i in range(26):
+        if freq[i] > 0:
+            count += freq[i]
+    
+    return count <= k
+    
+print(kAnagrams("apple", "peach", 1)) #Output: False
+print(kAnagrams("apple", "peach", 2)) #Output: True
+print(kAnagrams("cat", "dog", 3)) #Output: True
+print(kAnagrams("debit curd", "bad credit", 1)) #Output: True
+print(kAnagrams("baseball", "basketball", 2)) #Output: False
